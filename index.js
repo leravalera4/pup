abad81c085f4a27e1a1ce1512363f0ac43d04dd5
leravalera4 <51717886+leravalera4@users.noreply.github.com>
2024-11-21 20:17:15 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { scrapeLogic } = require("./in");
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/scrape", (req, res) => {
+  scrapeLogic(res);
+});
+
+app.get("/", (req, res) => {
+  res.send("Render Puppeteer server is up and running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+process.on('SIGINT', () => {
+  console.log("Процесс завершён");
+  process.exit(0);
+});
+
+setInterval(() => {
+  console.log("Работаю...");
+}, 10000);
